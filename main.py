c66cb96e212befd03be7522c42b9a15a25db2154
ISmolnyakov <smolnyakov.igor@gmail.com>
2026-03-03 15:35:30 +0700

    Shot feature added

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pygame
-from logger import log_state
+import sys
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from player import Player
+from shot import Shot
+from logger import log_state, log_event
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
 
 def main():
     pygame.init()
@@ -9,15 +15,48 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable) 
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable) 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Shot.containers = (shots, updatable, drawable) 
+    # projectile = Shot()
+
     while True:
         log_state()
-
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over")
+                sys.exit()
+
 
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
